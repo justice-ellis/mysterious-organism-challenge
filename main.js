95e45ee03011520ae2e63d797cb05575a09a0007
unknown <justice.ellis@amalitech.org>
2022-04-01 16:38:06 -0800

    mysterious-organism-challenge

---
+++ main.js
@@ -0,0 +1,81 @@
+// Return a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)] 
+  };
+  
+  // Return a random single stand of DNA containing 15 bases
+  const mockUpStrand = () => {
+    const newStrand = []
+    for (let i = 0; i < 15; i++) {
+      newStrand.push(returnRandBase())
+    }
+    return newStrand
+  };
+  
+  const pAequorFactory = (num, bases) => {
+    
+    // Declaration Of Return functions
+    return {
+      _specimenNum: num,
+      _dna: bases,
+      mutate() {  // Returns Object's dna
+        randomBase = Math.floor(Math.random() * 15);
+        dnaBasesNoA = ['T', 'C', 'G'];
+        dnaBasesNoT = ['A', 'C', 'G'];
+        dnaBasesNoC = ['A', 'T', 'G'];
+        dnaBasesNoG = ['A', 'T', 'C'];
+        if (this._dna[randomBase] === 'A') {
+          this._dna[randomBase] = dnaBasesNoA[Math.floor(Math.random() * 3)];
+        } else if (this._dna[randomBase] === 'T') {
+          this._dna[randomBase] = dnaBasesNoT[Math.floor(Math.random() * 3)];
+        } else if (this.dna[randomBase] === 'C') {
+          this._dna[randomBase] = dnaBasesNoC[Math.floor(Math.random() * 3)];
+        } else {
+          this._dna[randomBase] = dnaBasesNoG[Math.floor(Math.random() * 3)];
+        }
+        return this._dna;
+      },
+      compareDNA(pAequor) {   // Compare Current and past values of pAequor
+        numberCommonality = 0;
+        for (let i = 0; i < pAequor['dna'].length; i++) {
+          if (pAequor.dna[i] === this._dna[i]) {
+            numberCommonality++;
+          }
+        }
+        percentCommonality = ((numberCommonality / 15) * 100).toFixed(2);
+        console.log(`Specimen #${this._specimenNum} and Specimen #${pAequor._specimenNum} have ${percentCommonality}% DNA in common.`)
+      },
+      willLikelySurvive() {
+        cAndGCounter = 0;
+        for (let i = 0; i < this._dna.length; i++) {
+          if (this._dna[i] === 'C' || this._dna[i] === 'G') {
+            cAndGCounter++;
+          }
+        }
+        if (cAndGCounter < 9) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    };
+  };
+  
+  // Create 30 pAequor that are likely to survive
+  const survivors = [];
+  while (survivors.length < 30) {
+    pAequorNumber = survivors.length + 1;
+    newPAequor = pAequorFactory(pAequorNumber, mockUpStrand());
+    if (newPAequor.willLikelySurvive() === true) {
+      survivors.push(newPAequor);
+    }
+  }
+
+  // Call the function survivors
+  console.log(survivors);
+  
+
+
+ //---------------by: Ellis-Justice-Nana----------!
+  
